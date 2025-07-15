821b19195bd097741fa035f29607e09c533ea0f8
furium <shouldBnewB@gmail.com>
2025-07-15 11:01:51 +0900

    (add) 회원가입 페이지 및 FormField, Input 컴포넌트 추가, App 컴포넌트 라우팅 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import Landing from 'pages/landing';
+import LandingPage from 'pages/landing';
+import SignupPage from 'pages/signup';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route
-          path="/signup"
-          element={<div className="p-8 text-center text-forest">회원가입 페이지는 아직 구현되지 않았습니다.</div>}
-        />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signup" element={<SignupPage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
